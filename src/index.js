5f53bfb326bf5d3e3a715e7b4f127207a9ce6a56
Sanad <s_wj_88@hotmail.com>
2021-03-26 18:34:28 +0300

    Add thunk and logger to index

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
-import App from "./components/app";
-import reducers from "./reducers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/app';
+import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import './style/main.scss';
 
-import "./style/main.scss";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk, logger)));
 
 function main() {
   ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>,
-    document.querySelector(".app-wrapper")
+    document.querySelector('.app-wrapper'),
   );
 }
 
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', main);
