60a5d8471cfcdbf96f4241f7cd2f04aa30753790
Sanad <s_wj_88@hotmail.com>
2021-03-26 19:38:49 +0300

    Add registration reducer

---
+++ src/reducers/registrationReducer.js
@@ -0,0 +1,33 @@
+import { RegistrationActionTypes } from '../actions/actionTypes';
+
+const INITIAL_STATE = {
+  registration: {},
+  isFetching: false,
+  errorMessage: undefined,
+};
+
+const registrationReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case RegistrationActionTypes.FITCH_DATA_START:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case RegistrationActionTypes.FITCH_DATA_SUCCESS:
+      return {
+        ...state,
+        registration: action.registration,
+        isFetcheng: false,
+      };
+    case RegistrationActionTypes.FETCH_DATA_FAILURE:
+      return {
+        ...state,
+        errorMessage: action.error,
+        isFetching: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default registrationReducer;
