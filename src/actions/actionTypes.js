d8252002dc446b61ab50e3977c8a2f8c5d4c5b96
Sanad <s_wj_88@hotmail.com>
2021-04-03 22:57:44 +0300

    Add Nurse appointment action types
    Add Nurse appointment Actions

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -40,6 +40,12 @@ const NurseShowActionTypes = {
   FETCH_DATA_FAILURE: 'FETCH_DATA_FAILURE',
 };
 
+const NurseAppointmentActionTypes = {
+  FITCH_DATA_START: 'FETCH_DATA_START',
+  FITCH_DATA_SUCCESS: 'FETCH_DATA_SUCCESS',
+  FETCH_DATA_FAILURE: 'FETCH_DATA_FAILURE',
+};
+
 export {
   RegistrationActionTypes,
   AuthActionTypes,
@@ -48,4 +54,5 @@ export {
   DoctorAppointmentActionTypes,
   NurseSpecialtyActionTypes,
   NurseShowActionTypes,
+  NurseAppointmentActionTypes,
 };
---
+++ src/actions/appointment/nurseAppointmentActions.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { NurseAppointmentActionTypes } from '../actionTypes';
+
+export const fetchNurseAppointmentStart = () => ({
+  type: NurseAppointmentActionTypes.FITCH_DATA_START,
+});
+
+export const fetchNurseAppointmentSuccess = (appointment) => ({
+  type: NurseAppointmentActionTypes.FITCH_DATA_SUCCESS,
+  appointment,
+});
+
+export const fetchNurseAppointmentFailure = (error) => ({
+  type: NurseAppointmentActionTypes.FETCH_DATA_FAILURE,
+  error,
+});
+const token = localStorage.getItem('token');
+// eslint-disable-next-line arrow-body-style
+export const fetchNurseAppointmentStartAsync = (appointment) => {
+  return dispatch => {
+    dispatch(fetchNurseAppointmentStart());
+    axios
+      .post('http://localhost:5000/api/v1/create_nurse_appointment', appointment, {
+        headers: {
+          'Authorization': token
+        },
+      }, { withCredentials: false })
+      // eslint-disable-next-line max-len
+      .then((res) => dispatch(fetchNurseAppointmentSuccess(res.data), console.log(res.data)))
+      .catch((error) => dispatch(fetchNurseAppointmentFailure(error)));
+  };
+};
